68976f68fcdec0a82f52b82a5447dec667ed37d3
Bailey <vincenthtml@gmail.com>
2020-03-07 09:16:31 -0700

    init commit

---
+++ src/components/app/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Header from '../header/Header';
+
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        { id: 1, description: 'Stop students from using var.', completed: false },
+        { id: 2, description: 'Fix asynchronous swim.', completed: false },
+        { id: 3, description: 'Use only arrow function.', completed: true },
+        { id: 4, description: 'Create a React exercise.', completed: true }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header />
+      </div>
+    );
+  }
+}
+export default App;
